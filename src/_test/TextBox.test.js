45fa0e5dcf2dd631c4c479aaee37a36ec6f020a0
Johnsavadkuhi <physitime@gmail.com>
2020-09-22 06:52:40 +0330

    test TextBox input changes

--- src/_test/TextBox.test.js
+++ src/_test/TextBox.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {   render } from "@testing-library/react"
+import {   fireEvent , render } from "@testing-library/react"
 import TextBox from '../components/Auth/TextBox';
 
 test("TextBox placeholder test ",()=>{
@@ -16,3 +16,10 @@ test ("TextBox default value test " , ()=>{
     const {getByDisplayValue } = render(<TextBox defaultValue="hi"/>)
     expect(getByDisplayValue("hi")).toBeInTheDocument()
 })
+
+test("TExtBox change value test " , ()=>{
+    const utils= render(<TextBox placeholder="email" type="email" /> )
+    const input = utils.getByPlaceholderText("email"); 
+    fireEvent.change(input , {target: {value:"myemail@gmail.com"}})
+     expect(input.value).toBe("myemail@gmail.com")
+}) 
