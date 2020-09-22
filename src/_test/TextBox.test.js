90ab27b822db81c8446e492eb78fd7dd5ee6aff4
Johnsavadkuhi <physitime@gmail.com>
2020-09-22 03:51:44 +0330

    remove unused code

--- src/_test/TextBox.test.js
+++ src/_test/TextBox.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  getQueriesForElement, render, screen } from "@testing-library/react"
+import {   render } from "@testing-library/react"
 import TextBox from '../components/Auth/TextBox';
 
 test("TextBox placeholder test ",()=>{
